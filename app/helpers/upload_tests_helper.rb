9ba64517e1769dd1083077af99ef5935c61b96d1
shimoch <mikadonoosoba2@gmail.com>
2019-09-23 14:36:41 +0900

    delete needless files

--- app/helpers/upload_tests_helper.rb
+++
@@ -1,2 +0,0 @@
-module UploadTestsHelper
-end
--- config/deploy.rb
+++ config/deploy.rb
@@ -15,8 +15,8 @@ set :unicorn_rack_env, "production"
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'https://github.com/shimoch-214/freemarket_sample_59a.git'
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-set :branch, 'automatic-deploy'
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+# set :branch, 'automatic-deploy'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
