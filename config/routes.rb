5fe5e4f4421786772a3d1055b64fe0cbd9a05d60
shimoch <mikadonoosoba2@gmail.com>
2019-09-24 10:43:52 +0900

    making resource for mypages and its controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  get 'mypages/show'
+  get 'mypages/todo'
+  get 'mypages/notification'
   devise_for :users
   resources :upload_tests, only: [:index, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
+
+  # mypage routings
+  resource :mypage, only: :show do
+    member do
+      get 'todo'
+      get 'notification'
+    end
+  end
 end
