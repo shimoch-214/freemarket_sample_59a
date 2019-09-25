9cdc3746c70e027c95f1dcf79eddb1b2b1384e0a
shimoch <mikadonoosoba2@gmail.com>
2019-09-25 11:28:35 +0900

    finish mypage markup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'mypages/show'
-  get 'mypages/todo'
-  get 'mypages/notification'
   devise_for :users
   resources :upload_tests, only: [:index, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
