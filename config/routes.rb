453dae2a8ef719375f9604db8bdd84bfb6b2992b
katosan0904 <gachikiritanpo@gmail.com>
2019-09-25 17:07:03 +0900

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_scope :user do
+    get 'signup/registration/info' => 'users/registrations#user_info', as: :user_registration_info
+    # get 'signup/registration' => 'users/registrations#user_number', as: :user_registration_number
+    # # get 'signup/registration' => 'users/registrations#user_adress', as: :user_registration_adress
+    # # get 'signup/registration' => 'users/registrations#user_payment', as: :user_registration_payment
+    # # get 'signup/registration' => 'users/registrations#user_complete', as: :user_registration_complete
+
+    
+  end
+
+  devise_for :users,controllers:{
+    registrations: 'users/registrations',
+    sessions:'users/sessions'
+  }
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
 end
