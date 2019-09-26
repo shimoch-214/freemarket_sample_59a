36fe4e07ad761295810a2f82ee62f38872c0f399
Shiiiin-san <s.miura.engineer@gmail.com>
2019-09-26 12:22:33 +0900

    マークアップ用でproducts#showにルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :upload_tests, only: [:index, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'products#index'
+  root 'products#show'
 
   # mypage routings
   resource :mypage, only: :show do
