09d75865e39ce3696f615c398d6c5faa3a825e86
shimoch <mikadonoosoba2@gmail.com>
2019-09-27 17:39:51 +0900

    prepare for image uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,16 +1,15 @@
-class ImageTestUploader < CarrierWave::Uploader::Base
+class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
   if Rails.env.development? || Rails.env.test?
     storage :file
   else
     storage :fog
   end
-
+  
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
@@ -27,7 +26,6 @@ class ImageTestUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
-  process resize_to_fit: [400,400]
   #
   # def scale(width, height)
   #   # do something
