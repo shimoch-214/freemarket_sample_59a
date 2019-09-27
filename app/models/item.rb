baf5c655405217bcf40bae00f8c2c087776e3404
shimoch <mikadonoosoba2@gmail.com>
2019-09-27 17:33:56 +0900

    adopt japanese enum

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,11 @@ class Item < ApplicationRecord
   belongs_to  :category
   belongs_to  :sizing
   enum        condition: {
-    
+    unused: 0,
+    fresh:  1,
+    normal: 2,
+    poor:   3,
+    dirty:  4,
+    bad:    5
   }
 end
