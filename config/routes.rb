e1edc7e48dbac6ff350f867d22021ad01a6d9e00
Shiiiin-san <s.miura.engineer@gmail.com>
2019-09-27 17:54:39 +0900

    ルーティングをブランチを切る前に修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :upload_tests, only: [:index, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'products#show'
+  root 'products#index'
 
   # mypage routings
   resource :mypage, only: :show do
