950ab5b32dff70587599891b8510c144338fe852
Shiiiin-san <s.miura.engineer@gmail.com>
2019-09-28 11:57:48 +0900

    productコントローラに関係するものをitemコントローラに置き換えた

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
 
   resources :upload_tests, only: [:index, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'products#index'
+  root 'item#index'
 
   # mypage routings
   resource :mypage, only: :show do
