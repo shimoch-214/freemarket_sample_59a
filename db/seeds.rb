b384b98f7ca23c50b428305b020f463785846075
Shiiiin-san <s.miura.engineer@gmail.com>
2019-09-29 15:00:20 +0900

    seedファイルにitemのダミーデータ生成を記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,7 @@ require 'csv'
 # end
 
 # category_roots = Category.roots
-# レディース
+# # レディース
 # ladies = category_roots[0]
 # ladies.children[0].children.each do |gc|
 #   gc.update(sizing_id: 1)
@@ -58,7 +58,7 @@ require 'csv'
 #   gc.update(sizing_id: 1)
 # end
 
-# メンズ
+# # メンズ
 # mens = category_roots[1]
 # mens.children[0].children.each do |gc|
 #   gc.update(sizing_id: 1)
@@ -82,7 +82,7 @@ require 'csv'
 #   gc.update(sizing_id: 1)
 # end
 
-# ベビー・キッズ
+# # ベビー・キッズ
 # baby_kids = category_roots[2]
 # baby_kids.children[0].children.each do |gc|
 #   gc.update(sizing_id: 69)
@@ -106,13 +106,13 @@ require 'csv'
 #   gc.update(sizing_id: 60)
 # end
 
-# 家電・スマホ・カメラ
+# # 家電・スマホ・カメラ
 # gadget = category_roots[7]
 # gadget.children[3].children[2].update(sizing_id: 86)
 # gadget.children[3].children[3].update(sizing_id: 86)
 # gadget.children[4].children[0].update(sizing_id: 75)
 
-# スポーツ・レジャー
+# # スポーツ・レジャー
 # sports = category_roots[8]
 # sports.children[7].children[0].update(sizing_id: 137)
 # sports.children[7].children[2].update(sizing_id: 12)
@@ -129,7 +129,7 @@ require 'csv'
 # sports.children[8].children[6].update(sizing_id: 1)
 # sports.children[8].children[7].update(sizing_id: 52)
 
-# 自動車・オートバイ
+# # 自動車・オートバイ
 # vehicle = category_roots[11]
 # vehicle.children[1].children.each do |gc|
 #   gc.update(sizing_id: 115)
@@ -139,60 +139,42 @@ require 'csv'
 # Category.where(name: "その他").update_all(sizing_id: nil)
 
 
-# Usersテーブル：ダミー登録
-3.times do |u|
-  User.create!(
-    email: Faker::Internet.email,
-    password: "asdfghjk"
-  )
-end
+# # Usersテーブル：ダミー登録
+# 3.times do |u|
+#   User.create!(
+#     email: Faker::Internet.email,
+#     password: "asdfghjk"
+#   )
+# end
 
-# itemsテーブル：ダミー登録
-item_num = 300
-item_num.times do |i|
-  item = Item.new(
-    price: rand(100..99999),
-    name: Faker::Name.name ,
-    category_id: rand(3..20),
-    sizing_id: rand(2..11),
-    description: Faker::Creature::Animal.name,
-    condition: rand(0..5),
-  )
-  item.build_transact(
-    seller_id: 1,
-    delivery_method: rand(0..8),
-    bearing: false,
-    ship_days: rand(0..2),
-    status: rand(0..5),
-    prefecture_id: rand(1..47)
-  )
-  if item.transact.status.to_i > 0
-    item.transact.buyer = User.find(2)
-  end
-
-  # item.transact.save!
-  binding.pry
-  item.save!
-
-  10.times do |img|
-    Image.create!(
-      name: "sample_image_gandam.jpg",
-      item_id: img
-    )
-  end
-end
-
-# transactsテーブル：ダミー登録
-# item_num.times do |t|
-#   Transact.create!(
-#     item_id: t,
+# # itemsテーブル：ダミー登録
+# image_file_path = Dir.glob('/Users/miurashintaro/projects/freemarket_sample_59a/app/assets/images/item_image_sample/*')
+# item_num = 300
+# item_num.times do |i|
+#   item = Item.create!(
+#     price: rand(100..99999),
+#     name: Faker::Name.name ,
+#     category_id: rand(3..20),
+#     sizing_id: rand(2..11),
+#     description: Faker::Creature::Animal.name,
+#     condition: rand(0..5)
+#   )
+#   item.create_transact!(
 #     seller_id: 1,
 #     delivery_method: rand(0..8),
-#     bearing: false,
+#     bearing: true,
 #     ship_days: rand(0..2),
 #     status: rand(0..5),
 #     prefecture_id: rand(1..47)
 #   )
-# end
+#   if item.transact.status.to_i > 0
+#     item.transact.buyer = User.find(2)
+#   end
 
-Category.where(name: "その他").update_all(sizing_id: nil)
+#   10.times do |img|
+#     image = Image.create!(
+#       name: File.open(image_file_path.sample),
+#       item_id: Item.last.id
+#       )
+#   end
+# end
