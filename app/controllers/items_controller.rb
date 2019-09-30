d3f95086b40613b514b42bb38d9404537f02a68a
Shiiiin-san <s.miura.engineer@gmail.com>
2019-09-30 15:10:04 +0900

    ビューには繰り返し処理のみ、コントローラ側でカテゴリ・ブランドの設定や商品を分類する機能を記述、

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,26 @@ class ItemsController < ApplicationController
   layout "application"
 
   def index
-    @items = Item.page(params[:page]).per(10).order("created_at DESC")
-    @category = Category.all
+    items = {}
+    # 人気のカテゴリー、ブランドの種類を設定
+    @popular_items = {
+      "カテゴリー" => ["レディース", "メンズ", "家電・スマホ・カメラ", "おもちゃ・ホビー・グッズ"],
+      "ブランド" => ["シャネル", "ルイヴィトン", "シュプリーム", "ナイキ"]
+    }
+    # 設定したカテゴリー、ブランドをハッシュキーにして、itemレコードを取得
+    @popular_items.each do |item_tag, genre|
+      if item_tag == "カテゴリー"
+        genre.each do |a_genre|
+          grandchild_ids = Category.where(name: a_genre).first.indirect_ids
+          items[a_genre] = Item.where(category_id: grandchild_ids).page(params[:page]).per(10).order("created_at DESC")
+        end
+      else 
+        genre.each do |a_genre|
+          items[a_genre] = Item.where(brand: a_genre).page(params[:page]).per(10).order("created_at DESC")
+        end
+      end
+    end
+    @items = items
   end
 
   def new
