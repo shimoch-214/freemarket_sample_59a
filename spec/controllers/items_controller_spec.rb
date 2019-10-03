ebbf906a7727a254a15c51156d060f5a6bc60f97
Shiiiin-san <s.miura.engineer@gmail.com>
2019-10-03 11:54:27 +0900

    商品が売れた際に表示するSOLDマークを実装

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -2,12 +2,31 @@ require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
 
-  describe 'GET #index' do
-    # it "@itemsに人気カテゴリー・ブランドのitemレコードが紐付いている" do
+  # describe 'GET #index' do
+  #   # it "@itemsに人気カテゴリー・ブランドのitemレコードが紐付いている" do
+  #     # ブランドテーブル実装後に記述
+  #   # end
+
+  #   it "indexテンプレートを表示する" do
+  #     get :index
+  #     expect(response).to render_template :index
+  #   end
+  # end
+
+  describe 'GET #show' do
+    # it "@itemにparams[:id]のitemレコードが紐付いている" do
       # ブランドテーブル実装後に記述
     # end
 
-    it "indexテンプレートを表示する" do
+    # it "@user_itemsにparams[:id]を出品したユーザーの他のitemレコードが紐付いている" do
+      # ブランドテーブル実装後に記述
+    # end
+
+    # it "@itemにparams[:id]のitemレコードが紐付いている" do
+      # ブランドテーブル実装後に記述
+    # end
+
+    it "showテンプレートを表示する" do
       get :index
       expect(response).to render_template :index
     end
