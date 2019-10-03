c5f8116ea7545ee95d7b7349072cfc1f7b67fec1
Shiiiin-san <s.miura.engineer@gmail.com>
2019-10-03 10:07:30 +0900

    小画像にマウスオーバーするとそれに対応した大画像にスクロールする機能を実装

--- app/assets/javascripts/item_show.js
+++ app/assets/javascripts/item_show.js
@@ -1,8 +1,14 @@
 $(function() {
-  console.log('ok')
-  $('.show-items__photo-size-child--list').on('mouseenter', function(){
-    console.log(this)
-    $('.show-items__photo-size-child--list').css('opacity', '.3');
-    $(this).css('opacity', '.9');
+  var parent_photo = $('.show-items__photo-size-parent--list')
+  var child_photo = $('.show-items__photo-size-child--list')
+  var photo_size = parent_photo.children().length
+  parent_photo.css({width: `${photo_size * 300 - 300}`});
+  $('.show-items__photo--child :first-child').css({opacity: .9});
+
+  child_photo.on('mouseenter', function(){
+    child_photo.css({opacity: .3});
+    $(this).css({opacity: .9});
+    var index_chilled = child_photo.index(this);
+    parent_photo.animate({left:`${index_chilled * -300}px`}, { duration: 'slow'});
   })
 })
