16e872f13c71410f4c86eb670d9276854b8064d5
katosan0904 <gachikiritanpo@gmail.com>
2019-10-03 17:39:54 +0900

    edit factorybot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,10 @@
 FactoryBot.define do
   factory :user do
+    nickname              {"ゆーか"}
     email                 {"yuka@gmail.com"}
     password              {"yuka1234"}
     password_confirmation {"yuka1234"}
     phone_number          {"09012345678"}
+        
   end
 end
