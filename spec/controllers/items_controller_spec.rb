18d0e1870243f65304b2b8d5a8d3a3a45f4249f0
Ryoma Yoshioka <r.yoshioka4@gmail.com>
2019-10-07 12:58:43 +0900

    コントローラ単体テスト

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe ItemsController, type: :controller do
   #   end
   # end
 
-  describe 'GET #show' do
+  # describe 'GET #show' do
     # it "@itemにparams[:id]のitemレコードが紐付いている" do
       # ブランドテーブル実装後に記述
     # end
@@ -26,10 +26,28 @@ RSpec.describe ItemsController, type: :controller do
       # ブランドテーブル実装後に記述
     # end
 
-    it "showテンプレートを表示する" do
-      get :index
-      expect(response).to render_template :index
-    end
-  end
+    # it "showテンプレートを表示する" do
+    #   get :index
+    #   expect(response).to render_template :index
+    # end
+  # end
 
+  describe 'DELETE #destroy' do
+    it 'itemテーブルからitemレコードを削除する' do
+      # binding.pry
+      item = build(:item)
+      item.save
+      delete :destroy, params: {id: item}
+      expect{ item.destroy }.to change{Item.count}.by(-1)
+    end
+
+    # it '商品削除後にマイページに遷移する'
+    # expect( response ).to redirect_to(mypage_path)
+    # end
+    # it 'itemを削除するとimagesテーブルの紐づくレコードも削除される' do
+    # end
+    # it 'itemを削除するとimagesテーブルの紐づくレコードも削除される' do
+    # end
+
+  end
 end
