e15b63d4df37d5be6dc77f22d1906bcbddfc9d5f
Ryoma Yoshioka <r.yoshioka4@gmail.com>
2019-10-10 17:00:57 +0900

    リンク修正

--- app/controllers/transacts_controller.rb
+++ app/controllers/transacts_controller.rb
@@ -28,8 +28,8 @@ class TransactsController < ApplicationController
       @item = set_item
       Payjp.api_key = 'sk_test_1d97aaf5c495d2023d92a2bf'
       @card = Card.where(user_id: current_user.id).first
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
       @card_brand = @default_card_information.brand      
       case @card_brand
         when "Visa"
