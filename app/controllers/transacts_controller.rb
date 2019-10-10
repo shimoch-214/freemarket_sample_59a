b097b7df2a66bd830ef409a5fa24b6016eb8be15
Ryoma Yoshioka <r.yoshioka4@gmail.com>
2019-10-10 17:49:24 +0900

    指摘箇所修正２

--- app/controllers/transacts_controller.rb
+++ app/controllers/transacts_controller.rb
@@ -30,25 +30,29 @@ class TransactsController < ApplicationController
       @card = Card.where(user_id: current_user.id).first
       customer = Payjp::Customer.retrieve(@card.customer_id)
       @default_card_information = customer.cards.retrieve(@card.card_id)
-      @card_brand = @default_card_information.brand      
-      case @card_brand
-        when "Visa"
-          @card_src = "visa.svg"
-        when "JCB"
-          @card_src = "jcb.svg"
-        when "MasterCard"
-          @card_src = "master-card.svg"
-        when "American Express"
-          @card_src = "american_express.svg"
-        when "Diners Club"
-          @card_src = "dinersclub.svg"
-        when "Discover"
-          @card_src = "discover.svg"
-      end
+      set_brand
     end
   end
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def set_brand
+    @card_brand = @default_card_information.brand      
+    case @card_brand
+      when "Visa"
+        @card_src = "visa.svg"
+      when "JCB"
+        @card_src = "jcb.svg"
+      when "MasterCard"
+        @card_src = "master-card.svg"
+      when "American Express"
+        @card_src = "american_express.svg"
+      when "Diners Club"
+        @card_src = "dinersclub.svg"
+      when "Discover"
+        @card_src = "discover.svg"
+    end
   end
 end
