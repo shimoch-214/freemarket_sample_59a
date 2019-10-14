f5ec51064d10df4c6165095e7cfe520f6ebf5462
shimoch <mikadonoosoba2@gmail.com>
2019-10-14 13:34:57 +0900

    modify side bar actions

--- app/helpers/mypages_helper.rb
+++ app/helpers/mypages_helper.rb
@@ -1,5 +1,5 @@
 module MypagesHelper
-  
+
   def set_active_notification
     %w(show notification).include?(action_name) ? 'active' : ''
   end
@@ -7,4 +7,13 @@ module MypagesHelper
   def set_active_todo
     action_name == 'todo' ? 'active' : ''
   end
+
+  def side_bar_active(link)
+    link == request.path ? 'active' : ''
+  end
+
+  def side_bar_arrow_active(link)
+    link == request.path ? 'arrow-active' : ''
+  end
+
 end
